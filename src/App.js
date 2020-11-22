cc896c538c6460d198f8a58196da4625882ea78c
gurjeetsinghvirdee <gurjeetsinghvirdee@gmail.com>
2020-11-23 00:29:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
+    // BEM
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Spotify Clone</h1>
+
+      {/* Spotify Logo */}
+      {/* Login with spotify button */}
+      <Login />
+    </div> 
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="login">
+            <h1>Login page</h1>
+            {/* Spotify Logo */}
+            {/* Login with spotify button */}
+        </div>
+    )
+}
+
+export default Login
